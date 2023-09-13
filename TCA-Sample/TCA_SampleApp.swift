3015f56d5825591ec9f5f00e5fe936d09a04be25
Ibuki Onishi <ionishi@yahoo-corp.jp>
2023-09-13 21:57:07 +0900

    Initial Commit

---
+++ TCA-Sample/TCA_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_SampleApp.swift
+//  TCA-Sample
+//
+//  Created by ionishi on 2023/09/13.
+//
+
+import SwiftUI
+
+@main
+struct TCA_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
